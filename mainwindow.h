f1f5b6a609652205437c0217a4cfc7e45dc33b68
Henry Wang <126385991+HenryLiu000@users.noreply.github.com>
2023-05-27 03:23:04 +0800

    Add files via upload

--- mainwindow.h
+++ mainwindow.h
@@ -4,7 +4,8 @@
 #include <QMainWindow>
 #include <QDockWidget>
 #include <QtMultimedia/QSoundEffect>
-
+#include <QSound>
+#include "MyPushButton.h"
 //QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 //QT_END_NAMESPACE
@@ -15,9 +16,21 @@ class MainWindow : public QMainWindow
 
 public:
     MainWindow(QWidget *parent = nullptr);
+    //void paintEvent(QPaintEvent *event);
     ~MainWindow();
+
 
 private:
     Ui::MainWindow *ui;
+public slots:
+    void Start_Clicked();
+    void Setting_Clicked();
+    void Rule_Clicked();
+    void Quitgame_Clicked();
+    void QuitChooseMode_Clicked();
+    void Guy2Choose();
+    void Guy4Choose();
+    void Guy2ChooseReturnToMainMenu();
+    void Guy2ChooseReturnToChooseMode();
 };
 #endif // MAINWINDOW_H
